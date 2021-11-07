14f60fdca0bca6eb6c3548790dae408ed2d35f9c
jewelcse <jewelcse045@gmail.com>
2021-11-07 12:33:46 +0600

    endpoint updated

--- src/main/java/com/orderservice/controller/OrderController.java
+++ src/main/java/com/orderservice/controller/OrderController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("api/order-service/")
+@RequestMapping("api/v1/order-service/")
 public class OrderController {
 
     private OrderService orderService;
